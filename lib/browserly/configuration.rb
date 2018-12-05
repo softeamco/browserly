a29ebee5d2eadb9f0d0619e9d13f83bdb134b64e
Alexander Selivanov <selivandex@gmail.com>
2018-12-05 17:28:36 -0500

    gitignore; configuration pool_size added

--- lib/browserly/configuration.rb
+++ lib/browserly/configuration.rb
@@ -1,9 +1,10 @@
 module Browserly
   class Configuration
-    attr_accessor :tmp_path
+    attr_accessor :tmp_path, :pool_size
 
     def initialize
       @tmp_path = '../../tmp/screenshots'
+      @pool_size = 1
     end
   end
 end
